20d674cc6b5d5a2a42645592b34042e0e785f7bf
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-12-19 22:39:56 +0100

    2nd iteration pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,7 +2,12 @@ import string
 
 
 def is_pangram(sentence):
-    for char in string.ascii_lowercase:
-        if char not in sentence.lower():
-            return False
-    return True
+    try:
+        sentence.lower()
+    except AttributeError:
+        raise Exception('Thats not a proper sentence!')
+    else:
+        for char in string.ascii_lowercase:
+            if char not in sentence.lower():
+                return False
+        return True
