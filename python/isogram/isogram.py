8e619efa71fcc1927bf63dcff785679d18498ad8
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-12-19 22:22:24 +0100

    add nice(r) solution link

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,7 @@
 def is_isogram(string):
+    """
+    Interesting solution: http://exercism.io/submissions/dfa96d91e24d4176a0cfb9c76da72806
+    """
     chars = []
     for s in string.replace(' ', '').replace('-', '').lower():
         if s in chars:
