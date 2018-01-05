640ed94e09a9491c55b7bccca1a1914f18d5a1fa
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-01-06 00:12:33 +0100

    more efficient implementation

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,28 +1,15 @@
 class Allergies(object):
 
     def __init__(self, score):
-        items = {
-            1: 'eggs',
-            2: 'peanuts',
-            4: 'shellfish',
-            8: 'strawberries',
-            16: 'tomatoes',
-            32: 'chocolate',
-            64: 'pollen',
-            128: 'cats',
-        }
-        self.allergies = []
-        for i in [1024, 512, 256] + sorted(items, reverse=True):
-            if i <= score:
-                score -= i
-                try:
-                    self.allergies.append(items[i])
-                except KeyError:
-                    pass  # ignore higher items
+        self.score = score
+        self.table = dict(
+            eggs=1, peanuts=2, shellfish=4, strawberries=8, tomatoes=16,
+            chocolate=32, pollen=64, cats=128
+        )
 
     def is_allergic_to(self, item):
-        return item in self.allergies
+        return self.score & self.table[item] > 0
 
     @property
     def lst(self):
-        return self.allergies
+        return [k for k, v in self.table.items() if v & self.score > 0]
