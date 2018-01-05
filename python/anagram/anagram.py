9613d9f90f6d77bb195a7d051c98f487bb7f683c
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-01-05 22:51:29 +0100

    remove second loop

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,11 +1,6 @@
 def detect_anagrams(word, candidates):
     anagrams = []
     for candidate in candidates:
-        match = 0
-        if len(word) == len(candidate) and word.lower() != candidate.lower():
-            for char in word:
-                if word.lower().count(char) == candidate.lower().count(char):
-                    match += 1
-            if match == len(word):
-                anagrams.append(candidate)
+        if word.lower() != candidate.lower() and sorted(word.lower()) == sorted(candidate.lower()):
+            anagrams.append(candidate)
     return anagrams
