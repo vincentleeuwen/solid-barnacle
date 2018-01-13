aacde6e714da6760bd28b3eea271bc101e8492cf
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-01-13 22:11:53 +0100

    update largest series

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -3,14 +3,7 @@ from functools import reduce
 
 
 def largest_product(series, size):
-    if size == 0:
-        return 1
-    products = []
-    for i, s in enumerate(list(series)):
-        try:
-            subset = [int(obj) for obj in series[i:i + size]]
-            if len(subset) == size:
-                products.append(reduce(mul, subset, 1))
-        except IndexError:
-            continue
-    return max(products)
+    if size < 0:
+        raise ValueError('Incorrect input')
+    groups = [series[i:i + size] for i in range(len(series) - size + 1)]
+    return max(reduce(mul, map(int, group), 1) for group in groups)
